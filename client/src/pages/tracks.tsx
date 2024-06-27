2beef89d4f8b3b45aefdd5ca05d278cc152b1e44
Sameer Khan <muhammadsameer2016@yahoo.com>
2024-06-27 17:21:37 +0500

    implementated graphql query from client

---
+++ client/src/pages/tracks.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Layout, QueryResult } from "../components";
+import { gql } from "../__generated__";
+import { useQuery } from "@apollo/client";
+import TrackCard from "../containers/track-card";
+
+
+
+/**
+ * Tracks Page is the Catstronauts home page.
+ * We display a grid of tracks fetched with useQuery with the TRACKS query
+ */
+
+
+
+const TRACKS = gql(`
+  query GetTracks {
+  tracksForHome {
+    id
+    title
+    thumbnail
+    author {
+      id
+      name
+      photo
+    }
+    length
+    modulesCount
+  }
+}
+`)
+
+
+const Tracks = () => {
+  const { loading, error, data } = useQuery(TRACKS)
+
+  if (loading) return "Loading...";
+  if (error) return `Error ${error.message}`;
+  return <Layout grid>
+    <QueryResult error={error} loading={loading} data={data}>
+      {data?.tracksForHome?.map((track) => (
+        <TrackCard key={track?.id} track={track} />
+      ))}
+    </QueryResult>
+  </Layout>;
+};
+
+export default Tracks;
